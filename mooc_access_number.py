21a98ce069f48f1ce4bf437c3f47419c37a3c582
mengshouer <mengshouer@outlook.com>
2020-05-23 19:16:43 +0800

    fixlogin

--- mooc_access_number.py
+++ mooc_access_number.py
@@ -1,4 +1,4 @@
-import requests,time
+import requests,time,json,re,base64
 requests.packages.urllib3.disable_warnings()
 from io import BytesIO
 from PIL import Image,ImageDraw,ImageChops
@@ -11,20 +11,83 @@ password = ""   #登录密码
 s = requests.Session()
 s.headers.update({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'})
 
-def main():
-    getuserdata()
-    url = 'https://fystat-ans.chaoxing.com/log/setlog?personid='+cpi+'&courseId='+courseId+'&classId='+classId+'&encode=' +encode
-    i = 0
-    while 1:
+def captchalogin(username,password):
+    if(username == "" or password == ""):
+        username = input("登录账号：")
+        password = input("登录密码：")
+        
+    #以下两个用于自动识别验证码，手动输入验证码可无视
+    #百度云内的人工智能文字识别orc创建应用获得，不保证识别成功率，可以试试
+    APIKey = ""    
+    SecretKey = ""
+    #免费的每天有api限制
+    
+    if(APIKey != "" or SecretKey != ""):
+        getkeyurl = f'https://aip.baidubce.com/oauth/2.0/token'
+        data = {
+            "grant_type" : "client_credentials",
+            "client_id" : APIKey,
+            "client_secret" : SecretKey
+            }
+        getkey = requests.post(getkeyurl,data).text
+        access_token = json.loads(getkey)["access_token"]
+        numcode = ""
+        while 1:
+            t = int(round(time.time()*1000))
+            codeurl = f'http://passport2.chaoxing.com/num/code?'+ str(t)
+            img_numcode = s.get(codeurl).content
+            img = base64.b64encode(img_numcode)
+            orcurl = f'https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic?access_token='+access_token
+            data = {"image":img}
+            headers = {'content-type': 'application/x-www-form-urlencoded'}
+            captcha = requests.post(orcurl,data=data,headers=headers).text
+            numcodelen = json.loads(captcha)["words_result_num"]
+            if numcodelen == 0:
+                print("验证码识别错误，重新获取验证码识别")
+                time.sleep(1)
+            else:
+                numcode = json.loads(captcha)["words_result"][0]["words"]
+                numcode = re.sub("\D","",numcode)
+                if len(numcode) < 4:
+                    print("验证码识别错误，重新获取验证码识别")
+                    time.sleep(1)
+                else:
+                    print("识别成功")
+                    break
+    else:
+        t = int(round(time.time()*1000))
+        url = f'http://passport2.chaoxing.com/num/code?'+ str(t)
         web = s.get(url,verify=False)
-        time.sleep(5)
-        i+=1
-        print(i)
-        if(i == 500):
-            break
-    main()
+        img = Image.open(BytesIO(web.content))
+        img.show()
+        numcode = input('验证码：')
+    url = 'http://passport2.chaoxing.com/login?refer=http://i.mooc.chaoxing.com'
+    data = {'refer_0x001': 'http%3A%2F%2Fi.mooc.chaoxing.com',
+            'pid':'-1',
+            'pidName':'',
+            'fid':'1467', #院校id   1467:a系统
+            'fidName':'',
+            'allowJoin':'0',
+            'isCheckNumCode':'1',
+            'f':'0',
+            'productid':'',
+            'uname':username,
+            'password':password,
+            'numcode':numcode,
+            'verCode':''
+            }
+    web = s.post(url,data=data,verify=False)
+    time.sleep(2)
+    if('账号管理' in str(web.text)):
+        print('Login success!')
+        return s
+    else:
+        print('账号密码或验证码有误，请重试。')
+        username = ""
+        password = ""
+        captchalogin(username,password)
 
-
+'''
 def login():
     global uid,username,password
     if(username == "" or password == ""):
@@ -46,6 +109,7 @@ def login():
         username = ""
         password = ""
         login()
+'''
 
 
 def getuserdata():
@@ -88,18 +152,35 @@ def getuserdata():
     web = s.get(url)
     h2 = etree.HTML(web.text)
     encodeurl = h2.xpath('//script[@type = "text/javascript"]/@src')
-    url_query = urlparse(encodeurl[6]).query
+    i=0
+    for temp in range(0,len(encodeurl)):
+        if("encode" in encodeurl[i]):
+            break
+        i += 1
+    url_query = urlparse(encodeurl[i]).query
     userdata = dict([(k, v[0]) for k, v in parse_qs(url_query).items()])
     encode = userdata["encode"]
 
+def main():
+    getuserdata()
+    url = 'https://fystat-ans.chaoxing.com/log/setlog?personid='+cpi+'&courseId='+courseId+'&classId='+classId+'&encode=' +encode
+    i = 0
+    while 1:
+        web = s.get(url,verify=False)
+        time.sleep(5)
+        i+=1
+        print(i)
+        if(i == 500):
+            break
+    main()
     
 
 if __name__ == "__main__":
-    print("登录成功后等待访问数慢慢增加")
+    print("登录成功后等待访问数慢慢增加，显示的数字并不代表访问数，只是用于计数")
     try:
-        login()
+        captchalogin(username,password)
         main()
     except:
         print("登录信息尝试重新登录")
-        login()
+        captchalogin(username,password)
         main()
--- mooc_sign.py
+++ mooc_sign.py
@@ -1,5 +1,7 @@
-import requests,time,re,json
+import requests,time,json,re,base64
+from io import BytesIO
 requests.packages.urllib3.disable_warnings()
+from PIL import Image,ImageDraw,ImageChops
 from lxml import etree
 from urllib.parse import urlparse, parse_qs
 
@@ -11,29 +13,85 @@ s = requests.Session()
 s.headers.update({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'})
 activates = []
 
-def main():
-    getuserdata()
-    while 1:
-        url = f'https://mobilelearn.chaoxing.com/ppt/activeAPI/taskactivelist?courseId='+courseId+'&classId='+classId+'&uid='+str(uid)
-        web = s.get(url,verify=False)
-        activeList = json.loads(web.text)["activeList"]
-        for item in activeList:
-            if ("nameTwo" not in item):
-                continue
-            if (item['activeType'] == 2 and item['status'] == 1):
-                signurl = item['url']
-                var = signurl.split("&")
-                for var1 in var:
-                    var2 = var1.split("=")
-                    if(var2[0] == "activePrimaryId"):
-                        aid = var2[1]
-                if (aid not in activates):
-                    print("查询到签到 名称:%s 活动时间:%s aid:%s" % (
-                        item['nameOne'], item['nameFour'], aid))
-                    sign(aid, uid)
+def captchalogin(username,password):
+    if(username == "" or password == ""):
+        username = input("登录账号：")
+        password = input("登录密码：")
+        
+    #以下两个用于自动识别验证码，手动输入验证码可无视
+    #百度云内的人工智能文字识别orc创建应用获得，不保证识别成功率，可以试试
+    APIKey = ""    
+    SecretKey = ""
+    #免费的每天有api限制
+    
+    if(APIKey != "" or SecretKey != ""):
+        getkeyurl = f'https://aip.baidubce.com/oauth/2.0/token'
+        data = {
+            "grant_type" : "client_credentials",
+            "client_id" : APIKey,
+            "client_secret" : SecretKey
+            }
+        getkey = requests.post(getkeyurl,data).text
+        access_token = json.loads(getkey)["access_token"]
+        numcode = ""
+        while 1:
+            t = int(round(time.time()*1000))
+            codeurl = f'http://passport2.chaoxing.com/num/code?'+ str(t)
+            img_numcode = s.get(codeurl).content
+            img = base64.b64encode(img_numcode)
+            orcurl = f'https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic?access_token='+access_token
+            data = {"image":img}
+            headers = {'content-type': 'application/x-www-form-urlencoded'}
+            captcha = requests.post(orcurl,data=data,headers=headers).text
+            numcodelen = json.loads(captcha)["words_result_num"]
+            if numcodelen == 0:
+                print("验证码识别错误，重新获取验证码识别")
+                time.sleep(1)
             else:
-                time.sleep(3)
-                print("签到暂未开通")
+                numcode = json.loads(captcha)["words_result"][0]["words"]
+                numcode = re.sub("\D","",numcode)
+                if len(numcode) < 4:
+                    print("验证码识别错误，重新获取验证码识别")
+                    time.sleep(1)
+                else:
+                    print("识别成功")
+                    break
+    else:
+        t = int(round(time.time()*1000))
+        url = f'http://passport2.chaoxing.com/num/code?'+ str(t)
+        web = s.get(url,verify=False)
+        img = Image.open(BytesIO(web.content))
+        img.show()
+        numcode = input('验证码：')
+    url = 'http://passport2.chaoxing.com/login?refer=http://i.mooc.chaoxing.com'
+    data = {'refer_0x001': 'http%3A%2F%2Fi.mooc.chaoxing.com',
+            'pid':'-1',
+            'pidName':'',
+            'fid':'1467', #院校id   1467:a系统
+            'fidName':'',
+            'allowJoin':'0',
+            'isCheckNumCode':'1',
+            'f':'0',
+            'productid':'',
+            'uname':username,
+            'password':password,
+            'numcode':numcode,
+            'verCode':''
+            }
+    web = s.post(url,data=data,verify=False)
+    time.sleep(2)
+    if('账号管理' in str(web.text)):
+        print('Login success!')
+        for key, value in s.cookies.items():
+            if key == "_uid":
+                global uid
+                uid=value
+        return s
+    else:
+        print('账号密码或验证码有误，请重试。')
+        username = ""
+        password = ""
+        captchalogin(username,password)
 
 def sign(aid, uid):
     global activates
@@ -85,7 +143,8 @@ def sign(aid, uid):
             taskdata = h1.xpath('//dd[@class = "green"]/text()')
             time.sleep(3)
     '''
-            
+
+'''           
 def login():
     global uid,username,password
     if(username == "" or password == ""):
@@ -107,7 +166,7 @@ def login():
         username = ""
         password = ""
         login()
-
+'''
 
 def getuserdata():
     web = s.get('http://mooc1-1.chaoxing.com/visit/courses')
@@ -144,12 +203,35 @@ def getuserdata():
     global courseId, classId
     courseId = userdata["courseId"]
     classId = userdata["clazzid"]
-    
+
+def main():
+    getuserdata()
+    while 1:
+        url = f'https://mobilelearn.chaoxing.com/ppt/activeAPI/taskactivelist?courseId='+courseId+'&classId='+classId+'&uid='+str(uid)
+        web = s.get(url,verify=False)
+        activeList = json.loads(web.text)["activeList"]
+        for item in activeList:
+            if ("nameTwo" not in item):
+                continue
+            if (item['activeType'] == 2 and item['status'] == 1):
+                signurl = item['url']
+                var = signurl.split("&")
+                for var1 in var:
+                    var2 = var1.split("=")
+                    if(var2[0] == "activePrimaryId"):
+                        aid = var2[1]
+                if (aid not in activates):
+                    print("查询到签到 名称:%s 活动时间:%s aid:%s" % (
+                        item['nameOne'], item['nameFour'], aid))
+                    sign(aid, uid)
+            else:
+                time.sleep(3)
+                print("签到暂未开通")    
 
 if __name__ == "__main__":
     print("登录成功后等待签到开始")
     try:
-        login()
+        captchalogin(username,password)
         main()
     except Exception as e:
         print('repr(e):', repr(e))
